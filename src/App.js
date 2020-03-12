28f52a36b2953ec879a98e20d6f1c7ff372aad7e
Muhammad ALsaied <muhammadalsaied96@gmail.com>
2020-03-12 22:23:13 +0200

    add text

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hi there, I am Muhammad.
+          Edit on featue branch.
         </p>
         <a
           className="App-link"
